d28ba200decb09882bcd4955e4fea66f33895828
Nayanp35 <prajapatinayan35@gmail.com>
2022-09-29 18:50:30 +0000

    finished #3

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 //Code your solutions in this file
+//1
 function fiveToOneHundred(){
-    for (let i =5; i <= 100; i++){
+    for (let i = 5; i <= 100; i++){
         console.log(i);
     }
 }
 
 // fiveToOneHundred();
+
+//3 
+
+const multiplesOfThreeOrFive = () => {
+    for (let i = 3; i <= 100; i++) {
+        if (i % 3 === 0 || i % 5 ===0) {
+            console.log(i);
+        }
+    }
+}
+
+// multiplesOfThreeOrFive();
